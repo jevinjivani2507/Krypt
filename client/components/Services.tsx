3f8e91f63cd6518bbfee78a9128f810fabe0f973
Jevin Jivani <jivani.jevin25@gmail.com>
2022-12-27 16:57:37 +0530

    Footer ADDED

--- client/components/Services.tsx
+++ client/components/Services.tsx
@@ -10,7 +10,7 @@ interface ServiceCardProps {
 }
 
 const ServiceCard = ({ color, title, icon, subtitle }: ServiceCardProps) => (
-  <div className="flex flex-row justify-start items-start white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
+  <div className="flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
     <div
       className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}
     >
@@ -28,7 +28,7 @@ const Services = () => {
     <div className="flex w-full justify-center items-center gradient-bg-services">
       <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
         <div className="flex-1 flex flex-col justify-start items-start">
-          <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient ">
+          <h1 className="text-white text-3xl sm:text-5xl py-2">
             Services that we
             <br />
             continue to improve
